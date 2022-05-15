dcdd9fe6d8381f30f82c16ce00d8af1b5861daec
cruz <cruz_ch96@outlook.com>
2022-05-15 17:01:34 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome ...</p>
+  <div v-if="showModal">
+    <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal" /> -->
+    <Modal theme="sale" @close="toggleModal">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Open Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "MY first vue",
+      header: "Sign up for the giveaway!",
+      text: "join us",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
